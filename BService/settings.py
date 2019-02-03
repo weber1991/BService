99255c88db0260ba542a3211210991b606b759c3
xxb25-weber <weber666@foxmail.com>
2019-02-03 09:23:01 +0800

    关闭debug模式，zzb时间有问题，待处理

--- BService/settings.py
+++ BService/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!@8hrf8c8n(r7wyjc)^)u+sy-f@8l=k+a)slcq0)#k)sw1gq2s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['*',]
 
